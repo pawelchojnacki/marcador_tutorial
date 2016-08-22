745ea1e439cb6e153a793a91339ffffe95eef7af
rosz <anna.roszman@gmail.com>
2016-08-22 22:56:21 +0200

    second manager added to the bookmarks, showing only public bookmarks

--- mi_pagina/marcador/models.py
+++ mi_pagina/marcador/models.py
@@ -1,11 +1,28 @@
+# encoding: utf-8
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.timezone import now
 
+class PublicBookmarkManager(models.Manager):
+    def get_queryset(self):
+        qs = super(PublicBookmarkManager, self).get_queryset
+        return qs.filter(is_public=True)
 
+@python_2_unicode_compatible
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
+    class Meta:
+        verbose_name = "tag"
+        verbose_name_plural = "tags"
+        ordering = ["name"]
 
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
 class Bookmark(models.Model):
     url = models.URLField()
     title = models.CharField("title", max_length=255)
@@ -18,7 +35,19 @@ class Bookmark(models.Model):
         related_name="bookmarks")
     tags = models.ManyToManyField(Tag, blank=True)
 
+    object = models.Manager()
+    public = PublicBookmarkManager()
+
     class Meta:
         verbose_name = "bookmark"
         verbose_name_plural = "bookmarks"
         ordering = ["-date_created"]
+
+    def __str__(self):
+        return "%s (%s)" % (self.title, self.url)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.date_created = now()
+        self.date_updated = now()
+        super(Bookmark, self).save(*args, **kwargs)
