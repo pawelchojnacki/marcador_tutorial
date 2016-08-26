be172e82238ec62e3dd221bdc18f612a86f89328
rosz <anna.roszman@gmail.com>
2016-08-26 14:56:13 +0200

    3 new constants added to setting.py file, crispy_forms added to installed_apps

--- mi_pagina/mi_pagina/settings.py
+++ mi_pagina/mi_pagina/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'marcador',
+    'crispy_forms'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -105,3 +106,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+
+LOGIN_URL = 'mysite_login'
+LOGOUT_URL = 'mysite_logout'
+LOGIN_REDIRECT_URL = 'marcador_bookmark_list'
